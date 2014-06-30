012274c7f7a5bd0eca3a7b5f6210830adbc31eda
obnoxious <obnoxious@dongcorp.org>
2014-06-30 16:15:56 -0700

    go

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import Skype4Py
 import web
 import time
+import re
 
 urls = (
     '/api/(.*)', 'resolve'
@@ -9,6 +10,14 @@ urls = (
 
 instance = Skype4Py.Skype()
 instance.Attach()
+
+def findIpAddresses(skype):
+	ipAddresses = []
+	logs = glob.glob('debug-*.log')
+	print logs
+	for log in logs:
+		lol = re.findall(r".*PresenceManager:.*live:doxing_3.*0x.*-d-s[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}:[0-9]{1,}-r", open(log, 'r'))
+		return lol
 
 def writeToLog(skype, instance=instance):
 	instance.Client.OpenUserInfoDialog(skype)
@@ -19,7 +28,9 @@ def writeToLog(skype, instance=instance):
 
 class resolve:
     def GET(self, skype):
-        return writeToLog(skype)
+        writeToLog(skype)
+	time.sleep(2)
+	return findIpAddresses(skype)
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
