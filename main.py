97900d0b22361506830d0441727a53e54b02d85d
obnoxious <obnoxious@dongcorp.org>
2014-06-30 15:58:36 -0700

    go

--- main.py
+++ main.py
@@ -9,12 +9,15 @@ urls = (
 instance = Skype4Py.Skype()
 instance.Attach()
 
-def resolveIt(skype, instance=instance):
+def writeToLog(skype, instance=instance):
 	instance.Client.OpenUserInfoDialog(skype)
+	instance.Hide()
+	instance.Focus()
+	return True
 
 class resolve:
     def GET(self, skype):
-        return resolveIt(skype)
+        return writeToLog(skype)
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
